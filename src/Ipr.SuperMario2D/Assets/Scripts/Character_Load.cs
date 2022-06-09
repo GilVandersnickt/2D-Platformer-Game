af5de33a6d1e24605bdff1c908335b4f8e79f6cd
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-06-09 21:41:59 +0200

    Add script for character load

---
+++ src/Ipr.SuperMario2D/Assets/Scripts/Character_Load.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Load : MonoBehaviour
+{
+    public GameObject[] Players;
+    void Start()
+    {
+        int selected = PlayerPrefs.GetInt("SelectedCharacter");
+        Instantiate(Players[selected], Vector2.zero, Quaternion.identity);
+    }
+}
