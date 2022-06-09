1aec43871b646817061af6d39a2231023ffebae3
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-06-09 16:13:43 +0200

    Update player move script

--- src/Ipr.SuperMario2D/Assets/Scripts/Player_Move.cs
+++ src/Ipr.SuperMario2D/Assets/Scripts/Player_Move.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class Player_Move : MonoBehaviour
 {
-    private bool playerDirectionRight = false;
     private float moveX;
-    public int playerSpeed = 10;
-    public int playerJumpForce = 1250;
-    public bool onGround;
+    public int PlayerSpeed = 10;
+    public int PlayerJumpForce = 1250;
+    public bool OnGround;
+    public float PlayerBaseDistance;
 
     // Update is called once per frame
     void Update()
@@ -21,63 +21,69 @@ public class Player_Move : MonoBehaviour
     {
         moveX = Input.GetAxis("Horizontal");
 
-        if(Input.GetButtonDown("Jump") && onGround)
+        if(Input.GetButtonDown("Jump") && OnGround)
         {
             Jump();
         }
 
-        if (moveX < 0.0f && playerDirectionRight == false)
+        if (moveX != 0)
         {
-            FlipPlayer();
+            GetComponent<Animator>().SetBool("IsWalking", true);
         }
-        else if (moveX > 0.0f && playerDirectionRight == true)
+        else
         {
-            FlipPlayer();
+            GetComponent<Animator>().SetBool("IsWalking", false);
+        }
+        if (moveX < 0.0f)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else if (moveX > 0.0f)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
         }
 
-        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * PlayerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
     }
 
     void Jump()
     {
-        GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpForce);
-        onGround = false;
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * PlayerJumpForce);
+        OnGround = false;
     }
 
-    void FlipPlayer()
-    {
-        playerDirectionRight = !playerDirectionRight;
-        Vector2 localScale = gameObject.transform.localScale;
-        localScale.x *= -1;
-        transform.localScale = localScale;
-    }
-
-    void OnCollisionEnter2D (Collision2D collision)
-    {
-        //if (collision.gameObject.tag.Equals("Ground"))
-        //{
-        //    onGround = true;
-        //}
-    }
     void PlayerRaycast ()
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down);
+        RaycastHit2D hitUp = Physics2D.Raycast(transform.position, Vector2.up);
+        if (hitUp.collider != null && hitUp.distance < PlayerBaseDistance && hitUp.collider.tag == "Box")
+        {
+            Destroy(hitUp.collider.gameObject);
+        }
+        
+        RaycastHit2D hitDown = Physics2D.Raycast(transform.position, Vector2.down);
+        //Debug.Log($"{hitDown.distance} + {PlayerBaseDistance} + {hitDown.collider.tag} xxxxxxxxxxxx");
 
-        if (hit != null && hit.collider != null && hit.distance < 0.9f && hit.collider.tag == "Enemy")
+        if (hitDown != null && hitDown.distance <= PlayerBaseDistance && hitDown.collider.tag != "Enemy")
+        {
+            //Debug.Log($"{hitDown.distance} + {PlayerBaseDistance} + {hitDown.collider.tag}");
+            OnGround = true;
+        }
+
+        if (hitDown.collider != null && hitDown.distance < PlayerBaseDistance && hitDown.collider.tag == "Enemy")
         {
             GetComponent<Rigidbody2D>().AddForce(Vector2.up * 100);
-            hit.collider.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.right *100);
-            hit.collider.gameObject.GetComponent<Rigidbody2D>().gravityScale = 8;
-            hit.collider.gameObject.GetComponent<BoxCollider2D>().enabled = false;
-            hit.collider.gameObject.GetComponent<Enemy_Move>().enabled = false;
-            Debug.Log("Killed enemy");
-            //Destroy(hit.collider.gameObject);
+            hitDown.collider.gameObject.GetComponent<Transform>().position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, -1);
+            hitDown.collider.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.right *100);
+            hitDown.collider.gameObject.GetComponent<Rigidbody2D>().gravityScale = 8;
+            hitDown.collider.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            hitDown.collider.gameObject.GetComponent<Enemy_Move>().enabled = false;
+            Debug.Log($"{gameObject.name} squished {hitDown.collider.gameObject.name}");
 
         }
-        if (hit != null && hit.collider != null && hit.distance < 0.9f && hit.collider.tag != "Enemy")
-        {
-            onGround = true;
-        }
+        //if (hitDown.collider != null && hitDown.distance < PlayerBaseDistance && hitDown.collider.tag != "Enemy")
+        //{
+        //    OnGround = true;
+        //}
 
     }
 }
