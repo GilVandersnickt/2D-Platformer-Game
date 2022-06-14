b69fce68e5f14ed1843b5dec1dd3fabe294fb955
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-06-15 01:01:53 +0200

    Update constants for player

---
+++ src/Ipr.SuperMario2D/Assets/Scripts/Constants/Player.cs
@@ -0,0 +1,9 @@
+﻿namespace Assets.Scripts.Constants
+{
+    public class Player
+    {
+        public const int PlayerSpeed = 10;
+        public const int PlayerJumpForce = 10;
+        public const float PlayerBaseDistance = 0.5f;
+    }
+}
