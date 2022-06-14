22fb62cc3e45188ad38e94937a7c7a0337dbfbd8
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-06-14 17:15:15 +0200

    Add interface for enemy service

---
+++ src/Ipr.SuperMario2D/Assets/Scripts/Interfaces/IEnemyService.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Interfaces
+{
+    public interface IEnemyService
+    {
+        public void SpawnEnemy(GameObject enemy);
+        public void SpawnEnemies(GameObject[] enemies);
+    }
+}
