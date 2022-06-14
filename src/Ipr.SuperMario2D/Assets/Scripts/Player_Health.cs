c126ca238f983fbdf8e5c25d01cfaefa8f4ae094
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-06-14 17:19:43 +0200

    Update player health script

--- src/Ipr.SuperMario2D/Assets/Scripts/Player_Health.cs
+++ src/Ipr.SuperMario2D/Assets/Scripts/Player_Health.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Player_Health : MonoBehaviour
 {
-    public int Health;
+    public int Health = 3;
     public float MaxDepth = -4;
+    public Image[] Hearts;
+    public Sprite FullHeart;
+    public Sprite EmptyHeart;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        Health = 100;
-    }
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    Health = 3;
+    //}
 
     // Update is called once per frame
     void Update()
@@ -21,10 +26,21 @@ public class Player_Health : MonoBehaviour
     }
     void CheckHealth()
     {
+        foreach (var heart in Hearts)
+        {
+            heart.sprite = EmptyHeart;
+        }
+        for (int i = 0; i < Health; i++)
+        {
+            Hearts[i].sprite = FullHeart;
+        }
+
         if (Health <= 0 || gameObject.transform.position.y < MaxDepth)
             Die();
     }
-
+    public void TakeDamage()
+    {
+    }
     void Die()
     {
         Debug.Log($"{gameObject.name} died");     
