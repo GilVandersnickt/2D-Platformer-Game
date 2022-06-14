7571e5cbade45193e84cc1d3978f352edb0a01b5
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-06-14 17:13:39 +0200

    Add interface for game service

---
+++ src/Ipr.SuperMario2D/Assets/Scripts/Interfaces/IGameService.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Interfaces
+{
+    public interface IGameService
+    {
+        public void UpdateUI(GameObject timeObject, GameObject scoreObject, GameObject gameOverScreen);
+        public void Start(GameObject player);
+        public void Play();
+        public void Pause(GameObject pauseScreen);
+        public void Resume(GameObject pauseScreen);
+        public void Replay();
+        public void Menu();
+    }
+}
