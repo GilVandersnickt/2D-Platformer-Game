d606a833d94212668d9ce24a8c3717d1c425d410
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-06-14 17:13:11 +0200

    Add interface for menu service

---
+++ src/Ipr.SuperMario2D/Assets/Scripts/Interfaces/IMenuService.cs
@@ -0,0 +1,11 @@
+﻿namespace Assets.Scripts.Interfaces
+{
+    public interface IMenuService
+    {
+        public void StartGame();
+        public void GoToCharacterSelect();
+        public void SelectCharacter(int player);
+        public void GoToSettings();
+        public void SaveSettings(int time);
+    }
+}
