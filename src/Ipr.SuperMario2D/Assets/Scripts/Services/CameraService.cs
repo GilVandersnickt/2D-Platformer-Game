f920faf46a03a00323e885face42517b54e384a8
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-06-14 17:14:40 +0200

    Add camera service

---
+++ src/Ipr.SuperMario2D/Assets/Scripts/Services/CameraService.cs
@@ -0,0 +1,21 @@
+﻿using Assets.Scripts.Interfaces;
+using UnityEngine;
+
+namespace Assets.Scripts.Services
+{
+    public class CameraService : ICameraService
+    {
+        private float xMin = 0;
+        private float xMax = 10;
+        private float yMin = 0;
+        private float yMax = 0;
+
+        public void MoveCamera(GameObject camera)
+        {
+            var player = GameObject.FindGameObjectWithTag(Constants.Tags.Player);
+            float x = Mathf.Clamp(player.transform.position.x, xMin, xMax);
+            float y = Mathf.Clamp(player.transform.position.y, yMin, yMax);
+            camera.transform.position = new Vector3(x, y, camera.transform.position.z);
+        }
+    }
+}
