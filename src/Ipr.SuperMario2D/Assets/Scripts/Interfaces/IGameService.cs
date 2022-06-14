1dc8992949fcd5ffdb92435df14876292905edf8
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-06-15 01:03:23 +0200

    Update interface for game service

--- src/Ipr.SuperMario2D/Assets/Scripts/Interfaces/IGameService.cs
+++ src/Ipr.SuperMario2D/Assets/Scripts/Interfaces/IGameService.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Scripts.Interfaces
 {
     public interface IGameService
     {
-        public void UpdateUI(GameObject timeObject, GameObject scoreObject, GameObject gameOverScreen);
-        public void Start(GameObject player);
+        public void UpdateUI(GameObject timeObject, GameObject scoreObject, GameObject gameOverScreen, Image[] hearts, Sprite emptyHeart, Sprite fullHeart);
+        public void UpdateData(int score, int health);
+        public void Start();
         public void Play();
         public void Pause(GameObject pauseScreen);
         public void Resume(GameObject pauseScreen);
