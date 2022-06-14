a5c4fe62911ea8c1824d409d422e464172c7ae50
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-06-14 17:17:04 +0200

    Add camera controller

---
+++ src/Ipr.SuperMario2D/Assets/Scripts/Controllers/Camera/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using Assets.Scripts.Interfaces;
+using UnityEngine;
+using Zenject;
+
+namespace Assets.Scripts.Controllers.Camera
+{
+    public class CameraController : MonoBehaviour
+    {
+        private ICameraService _cameraService;
+
+        [Inject]
+        public void Construct(ICameraService cameraService)
+        {
+            _cameraService = cameraService;
+        }
+        void LateUpdate()
+        {
+            _cameraService.MoveCamera(gameObject);
+        }
+    }
+}
