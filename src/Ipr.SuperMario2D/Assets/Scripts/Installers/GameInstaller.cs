7c44cbb37a7ee7ad35f5dd93cea1592e607e8546
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-06-14 17:15:58 +0200

    Add game installer class for dependency injection with zenject

---
+++ src/Ipr.SuperMario2D/Assets/Scripts/Installers/GameInstaller.cs
@@ -0,0 +1,22 @@
+﻿using Assets.Scripts.Entities;
+using Assets.Scripts.Interfaces;
+using Assets.Scripts.Services;
+using Zenject;
+
+namespace Assets.Scripts.Installers
+{
+    public class GameInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            // IOC
+            Container.Bind<IGameService>().To<GameService>().AsSingle();
+            Container.Bind<IPlayerService>().To<PlayerService>().AsSingle();
+            Container.Bind<IEnemyService>().To<EnemyService>().AsSingle();
+            Container.Bind<ICameraService>().To<CameraService>().AsSingle();
+            Container.Bind<IMenuService>().To<MenuService>().AsSingle();
+
+            Container.Bind<Player>().AsSingle();
+        }
+    }
+}
