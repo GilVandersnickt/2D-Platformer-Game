fb6849868e2684c59f450d48e7af5cc1c89fa55b
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-06-14 17:11:56 +0200

    Update constants with tags

---
+++ src/Ipr.SuperMario2D/Assets/Scripts/Constants/Tags.cs
@@ -0,0 +1,11 @@
+﻿namespace Assets.Scripts.Constants
+{
+    public class Tags
+    {
+        public const string Player = "Player";
+        public const string Enemy = "Enemy";
+        public const string Ground = "Ground";
+        public const string Box = "Box";
+        public const string Coin = "Coin";
+    }
+}
