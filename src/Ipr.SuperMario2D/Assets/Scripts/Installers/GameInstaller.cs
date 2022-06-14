07a48f0b2ebc6c3a25a3658301881f39bee50ebb
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-06-15 01:03:00 +0200

    Update game installer with factory for instantiating a player

--- src/Ipr.SuperMario2D/Assets/Scripts/Installers/GameInstaller.cs
+++ src/Ipr.SuperMario2D/Assets/Scripts/Installers/GameInstaller.cs
@@ -1,12 +1,16 @@
-﻿using Assets.Scripts.Entities;
+﻿using Assets.Scripts.Controllers.Player;
+using Assets.Scripts.Entities;
 using Assets.Scripts.Interfaces;
 using Assets.Scripts.Services;
+using UnityEngine;
 using Zenject;
 
 namespace Assets.Scripts.Installers
 {
     public class GameInstaller : MonoInstaller
     {
+        public PlayerController[] playerPrefabs;
+
         public override void InstallBindings()
         {
             // IOC
@@ -17,6 +21,14 @@ namespace Assets.Scripts.Installers
             Container.Bind<IMenuService>().To<MenuService>().AsSingle();
 
             Container.Bind<Player>().AsSingle();
+
+            // Factory for instantiating a player
+            Container.BindFactory<PlayerController, PlayerController.Factory>().FromComponentInNewPrefab(playerPrefabs[GetCharacterIndex()]);
+        }
+
+        private int GetCharacterIndex()
+        {
+            return PlayerPrefs.GetInt(Constants.PlayerPrefsTitles.SelectedCharacter, 0) - 1;
         }
     }
 }
