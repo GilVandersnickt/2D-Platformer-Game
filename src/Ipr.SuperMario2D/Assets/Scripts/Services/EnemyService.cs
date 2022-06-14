9292f0938244f6f122813bd2ca5382989dcdb782
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-06-14 17:15:26 +0200

    Add enemy service

---
+++ src/Ipr.SuperMario2D/Assets/Scripts/Services/EnemyService.cs
@@ -0,0 +1,18 @@
+﻿using Assets.Scripts.Interfaces;
+using UnityEngine;
+
+namespace Assets.Scripts.Services
+{
+    public class EnemyService : IEnemyService
+    {
+        public void SpawnEnemy(GameObject enemy)
+        {
+            Object.Instantiate(enemy);
+        }
+        public void SpawnEnemies(GameObject[] enemies)
+        {
+            foreach (GameObject enemy in enemies)
+                Object.Instantiate(enemy);
+        }
+    }
+}
