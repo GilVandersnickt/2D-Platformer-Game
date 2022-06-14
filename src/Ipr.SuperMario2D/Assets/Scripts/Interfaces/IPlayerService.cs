524aefefc4a75db751b22053bf2d309ba1770f2f
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-06-14 17:14:07 +0200

    Add interface for player service

---
+++ src/Ipr.SuperMario2D/Assets/Scripts/Interfaces/IPlayerService.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Interfaces
+{
+    public interface IPlayerService
+    {
+        public void Move(Rigidbody2D playerRigidBody, Animator playerAnimator, Transform groundCheck, LayerMask groundLayer);
+        public int CheckHealth();
+        public void TakeCoin();
+        public void TakeDamage();
+        public void Die();
+        public Image[] UpdateHearts(Image[] heartsUI, Sprite emptyHeart, Sprite fullHeart);
+    }
+}
