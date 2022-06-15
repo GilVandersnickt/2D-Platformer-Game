5ccd8205c02d87491d5e753c436fa701a0d75314
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-06-15 07:10:00 +0200

    Clean enemy controller to use enemy service

--- src/Ipr.SuperMario2D/Assets/Scripts/Controllers/Enemy/EnemyController.cs
+++ src/Ipr.SuperMario2D/Assets/Scripts/Controllers/Enemy/EnemyController.cs
@@ -1,51 +1,25 @@
-﻿using UnityEngine;
+﻿using Assets.Scripts.Controllers.Game;
+using Assets.Scripts.Controllers.Player;
+using Assets.Scripts.Interfaces;
+using UnityEngine;
+using Zenject;
 
 namespace Assets.Scripts.Controllers.Enemy
 {
     public class EnemyController : MonoBehaviour
     {
-        private int enemySpeed;
-        private int xMoveDirection;
-        private int maxDepth;
-        private float hitDistance;
+        IEnemyService _enemyService;
 
-        void Start()
+        [Inject]
+        public void Construct(IEnemyService enemyService)
         {
-            enemySpeed = Constants.Enemy.EnemySpeed;
-            xMoveDirection = Constants.Enemy.XMoveDirection;
-            maxDepth = Constants.Enemy.MaxDepth;
-            hitDistance = Constants.Enemy.HitDistance;
+            _enemyService = enemyService;
         }
-        // Update is called once per frame
+
+
         void Update()
         {
-            Move();
-            Check();
+            _enemyService.Move(gameObject);
         }
-
-        private void Move()
-        {
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(xMoveDirection, 0));
-            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(xMoveDirection, 0) * enemySpeed;
-            if (hit.distance < hitDistance && hit.collider.tag.Equals(Constants.Tags.Ground))
-            {
-                Flip();
-            }
-        }
-        private void Flip()
-        {
-            if (xMoveDirection > 0)
-                xMoveDirection = -1;
-            else
-                xMoveDirection = 1;
-
-            transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
-        }
-        private void Check()
-        {
-            if (gameObject.transform.position.y < maxDepth)
-                Destroy(gameObject);
-        }
-
     }
 }
