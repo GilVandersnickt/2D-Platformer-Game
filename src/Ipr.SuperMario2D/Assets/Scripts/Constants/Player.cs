084db0b1c5cd51826f923c788cb1560752220b49
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-06-15 08:13:38 +0200

    Update constants player

--- src/Ipr.SuperMario2D/Assets/Scripts/Constants/Player.cs
+++ src/Ipr.SuperMario2D/Assets/Scripts/Constants/Player.cs
@@ -3,7 +3,7 @@
     public class Player
     {
         public const int PlayerSpeed = 10;
-        public const int PlayerJumpForce = 10;
+        public const int PlayerJumpForce = 12;
         public const float PlayerBaseDistance = 0.5f;
         public const int MaxDepth = -10;
     }
