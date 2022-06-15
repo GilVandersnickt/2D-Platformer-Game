33ef9e0b76f3a2f59a330e193eeec99e15fac75d
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-06-15 09:12:23 +0200

    Update constants for player

--- src/Ipr.SuperMario2D/Assets/Scripts/Constants/Player.cs
+++ src/Ipr.SuperMario2D/Assets/Scripts/Constants/Player.cs
@@ -6,5 +6,7 @@
         public const int PlayerJumpForce = 12;
         public const float PlayerBaseDistance = 0.5f;
         public const int MaxDepth = -10;
+        public const int HealthpackValue = 1;
+        public const int MaxHealth = 3;
     }
 }
