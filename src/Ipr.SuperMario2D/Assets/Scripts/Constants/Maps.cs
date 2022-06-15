f74411c08d66b0493104242a9a3b8333183452c7
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-06-15 09:12:12 +0200

    Add constants for maps

---
+++ src/Ipr.SuperMario2D/Assets/Scripts/Constants/Maps.cs
@@ -0,0 +1,10 @@
+﻿namespace Assets.Scripts.Constants
+{
+    public class Maps
+    {
+        public const int XMin = 0;
+        public const int XMax = 55;
+        public const int YMin = 0;
+        public const int YMax = 0;
+    }
+}
