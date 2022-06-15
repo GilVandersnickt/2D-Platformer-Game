826c587f21aca68b11d38d438d344d4674c14063
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-06-15 07:05:10 +0200

    Update enemy service with move method

--- src/Ipr.SuperMario2D/Assets/Scripts/Services/EnemyService.cs
+++ src/Ipr.SuperMario2D/Assets/Scripts/Services/EnemyService.cs
@@ -1,18 +1,63 @@
-﻿using Assets.Scripts.Interfaces;
+﻿using Assets.Scripts.Controllers.Game;
+using Assets.Scripts.Controllers.Player;
+using Assets.Scripts.Interfaces;
 using UnityEngine;
 
 namespace Assets.Scripts.Services
 {
     public class EnemyService : IEnemyService
     {
-        public void SpawnEnemy(GameObject enemy)
+        private int enemySpeed;
+        private int xMoveDirection;
+        private int maxDepth;
+        private float hitDistance;
+
+        public EnemyService()
         {
-            Object.Instantiate(enemy);
+            enemySpeed = Constants.Enemy.EnemySpeed;
+            xMoveDirection = Constants.Enemy.XMoveDirection;
+            maxDepth = Constants.Enemy.MaxDepth;
+            hitDistance = Constants.Enemy.HitDistance;
         }
-        public void SpawnEnemies(GameObject[] enemies)
+        public void Move(GameObject gameObject)
         {
-            foreach (GameObject enemy in enemies)
-                Object.Instantiate(enemy);
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(xMoveDirection, 0) * enemySpeed;
+
+            if (gameObject.transform.position.y < maxDepth)
+                Object.Destroy(gameObject);
+
+            CheckColliders(gameObject);
+        }
+        private void Flip(GameObject gameObject)
+        {
+            if (xMoveDirection > 0)
+                xMoveDirection = -1;
+            else
+                xMoveDirection = 1;
+
+            gameObject.transform.localScale = new Vector2(gameObject.transform.localScale.x * -1, gameObject.transform.localScale.y);
+        }
+
+        private void CheckColliders(GameObject gameObject)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(gameObject.transform.position, new Vector2(xMoveDirection, 0));
+            if (hit.distance > hitDistance) return;
+            switch (hit.collider.tag)
+            {
+                case Constants.Tags.Ground:
+                    Flip(gameObject);
+                    break;
+
+                case Constants.Tags.Player:
+                    hit.collider.GetComponent<PlayerController>().TakeDamage(hit.collider.GetComponent<Animator>());
+                    Debug.Log($"{hit.collider.name} got hit by {gameObject.name}! {GameController.Health} lives left");
+                    Flip(gameObject);
+                    break;
+
+                default:
+                    break;
+            }
+
         }
     }
 }
