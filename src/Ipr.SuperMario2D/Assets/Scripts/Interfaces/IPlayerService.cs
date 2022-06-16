4acf898aca628bb28bf7319574a402146b5416f4
gilvandersnickt <gil.vandersnickt@student.howest.be>
2022-06-16 02:21:30 +0200

    Update interface player service

--- src/Ipr.SuperMario2D/Assets/Scripts/Interfaces/IPlayerService.cs
+++ src/Ipr.SuperMario2D/Assets/Scripts/Interfaces/IPlayerService.cs
@@ -1,13 +1,18 @@
-﻿using UnityEngine;
+﻿using Assets.Scripts.Entities;
+using UnityEngine;
 
 namespace Assets.Scripts.Interfaces
 {
     public interface IPlayerService
     {
+        public void Play();
         public void Move(Rigidbody2D playerRigidBody, Animator playerAnimator, Transform groundCheck, LayerMask groundLayer);
         public void TakeCoin();
         public void TakeDamage();
         public void Die();
         public void Collide(Collider2D collider);
+        public Player GetPlayer();
+        public void SetPlayer();
+        public void SetPlayer(Player player);
     }
 }
